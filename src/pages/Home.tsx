10776b1a7870a93d52b6f5544965be413d406cc0
BTA for testing <165192770+bta-tsotne@users.noreply.github.com>
2025-09-16 19:48:34 +0400

    Update Home.tsx: changed background to lightgray

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,7 +15,7 @@ export default function Home() {
   if (loading) return <p className="text-center">Loading products...</p>;
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 p-6">
+    <div className="bg-gray-200 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 p-6">
       {items.map(product => (
         <ProductCard key={product.id} product={product} />
       ))}
