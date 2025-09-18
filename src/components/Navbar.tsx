f68918648756cd684717d7603a1d8dd10bb02084
BTA for testing <165192770+bta-tsotne@users.noreply.github.com>
2025-09-18 17:51:56 +0400

    Update Navbar.tsx style: darkblue background

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   const cartItems = useAppSelector(state => state.cart.items);
 
   return (
-    <nav className="bg-gray-100 border-stone-700 border-b-4 text-gray-950 px-6 py-12 flex justify-between items-center">
+    <nav className="bg-blue-800 border-stone-700 border-b-4 text-gray-100 px-6 py-12 flex justify-between items-center">
       <Link to="/" className="text-xl">ონლაინ მაღაზია</Link>
       <div className="flex gap-6">
         <Link to="/">საწყისი</Link>
