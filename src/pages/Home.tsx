d6e52645f0a685c8116e655aae1e678981b509fd
Tsotne Chaduneli <86349055+tsotne-chaduneli@users.noreply.github.com>
2025-09-18 19:19:05 +0400

    Home.tsx background to violet

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,7 +15,7 @@ export default function Home() {
   if (loading) return <p className="text-center">Loading products...</p>;
 
   return (
-    <div className="bg-emerald-100 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 p-6">
+    <div className="bg-violet-200 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 p-6">
       {items.map(product => (
         <ProductCard key={product.id} product={product} />
       ))}
